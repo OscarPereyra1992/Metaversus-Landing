1bd597f5f4eb5cc0ce1114e780d2ff219ac919e1
OscarPereyra1992 <oscarpereyra.mhs@gmail.com>
2023-08-26 15:44:52 -0300

    build changes

--- sections/World.jsx
+++ sections/World.jsx
@@ -28,19 +28,19 @@ const World = () => (
 
       <motion.div
         variants={fadeIn('up', 'tween', 0.3, 1)}
-        className="relative mt-[68px] flex w-full h-[550px]"
+        className="relative mt-[68px] flex w-full"
       >
         <img src="/map.png" alt="map" className="w-full h-full object-cover" />
 
-        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+        <div className="absolute bottom-[16%] right-[8%] w-[40px] md:w-[50px] lg:w-[70px] h-[40px] md:h-[50px] lg:h-[70px] p-[6px] rounded-full bg-[#5d6680]">
           <img src="people-01.png" alt="people" className="w-full h-full" />
         </div>
 
-        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+        <div className="absolute top-[10%] left-[10%] w-[40px] md:w-[50px] lg:w-[70px] h-[40px] md:h-[50px] lg:h-[70px] p-[6px] rounded-full bg-[#5d6680]">
           <img src="/people-02.png" alt="people" className="w-full h-full" />
         </div>
 
-        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+        <div className="absolute top-[45%] left-[42%] w-[40px] md:w-[50px] lg:w-[70px] h-[40px] md:h-[50px] lg:h-[70px] p-[6px] rounded-full bg-[#5d6680]">
           <img src="people-03.png" alt="people" className="w-full h-full" />
         </div>
       </motion.div>
